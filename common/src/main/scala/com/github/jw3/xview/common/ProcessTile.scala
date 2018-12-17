61158831a565b57a88b50648aa6385f9f87d026c
John Wass <wassj@ctc.com>
2018-12-17 01:22:09 -0500

    write single label file

--- common/src/main/scala/com/github/jw3/xview/common/ProcessTile.scala
+++ common/src/main/scala/com/github/jw3/xview/common/ProcessTile.scala
@@ -5,18 +5,18 @@ import akka.Done
 import akka.actor.{ActorRef, ActorSystem}
 import akka.stream.Materializer
 import akka.stream.alpakka.s3.S3Settings
+import akka.stream.alpakka.s3.impl.S3Headers
 import akka.stream.alpakka.s3.scaladsl.S3Client
 import akka.stream.scaladsl.{FileIO, JsonFraming, Sink, Source}
 import akka.util.ByteString
 import com.github.jw3.xview.common.MakeChips.{FChip, FeatureData, _}
-import geotrellis.raster
 import geotrellis.raster.RasterExtent
 import geotrellis.raster.io.geotiff.MultibandGeoTiff
 import geotrellis.raster.io.geotiff.reader.GeoTiffReader
 import geotrellis.vector.io.json.FeatureFormats._
 import geotrellis.vector.io.json.GeoJson
 import geotrellis.vector.io.json.GeometryFormats._
-import geotrellis.vector.{Extent, Feature, Polygon}
+import geotrellis.vector.{Feature, Polygon}
 
 import scala.concurrent.Future
 import scala.util.{Failure, Success}
@@ -37,6 +37,11 @@ object ProcessTile {
     case None ⇒ s"$t.tif"
   }
   def jsonFile(t: Int, path: Option[String]): String = tifFile(t, path) + ".geojson"
+
+  def labelFile(t: Int, path: Option[String]): String = path match {
+    case Some(p) ⇒ s"$p/$t.txt"
+    case None ⇒ s"$t.txt"
+  }
 
   def dl_tif(tile: Int, from: S3Path, to: Path)(implicit mat: Materializer, s3: S3Client) =
     s3.download(from.bucket, tifFile(tile, from.path))._1.runWith(FileIO.toPath(to))
@@ -88,18 +93,21 @@ object ProcessTile {
         .map(bs ⇒ GeoJson.parse[Feature[Polygon, FeatureData]](bs.utf8String))
         .filter(f ⇒ filter.isEmpty || filter.contains(f.data.type_id))
         .map { fd ⇒
-          val re = RasterExtent(Extent(fd.geom.envelope.jtsEnvelope), tif.cellSize)
-          val l = KittiLabel(s"${fd.data.type_id}", 0, 0, re.cols, re.rows)
-          FLabel(fd, KittiLabel.toRow(l))
+          val re = RasterExtent(tif.extent, tif.cellSize)
+          val env = fd.geom.envelope
+          val min = re.mapToGrid(env.xmin, env.ymin)
+          val max = re.mapToGrid(env.xmax, env.ymax)
+
+          val l = YoloLabel(s"${fd.data.type_id}", min._1, min._2, max._1, max._2)
+          FLabel(fd, YoloLabel.toRow(l))
         }
-        .map(c ⇒ c.f → ByteString(c.l))
-        .mapAsync(1)(
-          t ⇒
-            Source
-              .single(t._2)
-              .runWith(s3Client
-                .multipartUpload(to.bucket,
-                                 chippedFileWithType(num, t._1.data.feature_id, "txt", t._1.data.type_id, to.path))))
+        .scan(StringBuilder.newBuilder) { (x, y) ⇒
+          x ++= y.l
+        }
+        .map(s ⇒ ByteString(s.mkString))
+        .mapAsync(1)(t ⇒
+          s3Client
+            .putObject(to.bucket, labelFile(num, to.path), Source.single(t), t.length, s3Headers = S3Headers.empty))
         .runWith(Sink.ignore)
       f.onComplete {
         case Success(_) if ref != ActorRef.noSender ⇒ ref ! Complete(num)
